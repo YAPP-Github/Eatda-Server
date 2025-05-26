5a60c0227d6bb4cb3b250f66c985c18be67e32bc
leegwichan <cnddkscndgus@naver.com>
2025-05-26 17:18:08 +0900

    init : 초기 프로젝트 설정

---
+++ src/main/java/baegam/sundae/SundaeApplication.java
@@ -0,0 +1,12 @@
+package baegam.sundae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundaeApplication.class, args);
+	}
+}
---
+++ src/test/java/baegam/sundae/SundaeApplicationTests.java
@@ -0,0 +1,13 @@
+package baegam.sundae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
