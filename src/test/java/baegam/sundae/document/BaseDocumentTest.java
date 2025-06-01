e0618a4a8b9417c7c60224fab912c0cb798eb778
leegwichan <cnddkscndgus@naver.com>
2025-06-01 17:52:19 +0900

    feat : 문서 관련 테스트 세팅

---
+++ src/test/java/baegam/sundae/document/BaseDocumentTest.java
@@ -0,0 +1,55 @@
+package baegam.sundae.document;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.restassured.RestAssuredRestDocumentation;
+import org.springframework.restdocs.restassured.RestAssuredRestDocumentationConfigurer;
+import org.springframework.restdocs.restassured.RestDocumentationFilter;
+
+@ExtendWith({RestDocumentationExtension.class, MockitoExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class BaseDocumentTest {
+
+    // @MockitoBean 을 이용하여 Service Layer 및 특정 객체 Mocking
+
+    @LocalServerPort
+    private int port;
+
+    private RequestSpecification spec;
+
+
+    @BeforeEach
+    void setEnvironment(RestDocumentationContextProvider restDocumentation) {
+        RestAssured.port = port;
+        RestAssuredRestDocumentationConfigurer webConfigurer =
+                RestAssuredRestDocumentation.documentationConfiguration(restDocumentation);
+        spec = new RequestSpecBuilder()
+                .addFilter(webConfigurer)
+                .build();
+    }
+
+    protected final RestDocsRequest request() {
+        return new RestDocsRequest();
+    }
+
+    protected final RestDocsResponse response() {
+        return new RestDocsResponse();
+    }
+
+    protected final RestDocsFilterBuilder document(String identifierPrefix, int statusCode) {
+        return new RestDocsFilterBuilder(identifierPrefix, Integer.toString(statusCode));
+    }
+
+    protected RequestSpecification given(RestDocumentationFilter documentationFilter) {
+        return RestAssured.given(spec)
+                .filter(documentationFilter);
+    }
+}
