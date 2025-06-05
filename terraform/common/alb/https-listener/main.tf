62b07c5f0388bb21a87e65b0efc04a79ab5af93a
lvalentine6 <ysr1227@naver.com>
2025-06-06 06:26:57 +0900

    feat(https-listener): ALB HTTPS 리스너 모듈 구현

---
+++ terraform/common/alb/https-listener/main.tf
@@ -0,0 +1,27 @@
+resource "aws_alb_listener" "https_listener" {
+  load_balancer_arn = var.alb_arn
+  port              = var.https_listener.port
+  protocol          = var.https_listener.protocol
+
+  default_action {
+    type             = var.https_listener.type
+    target_group_arn = var.https_listener.default_target_group_arn
+  }
+}
+
+resource "aws_alb_listener_rule" "listener_rules" {
+  for_each     = var.listener_rules
+  listener_arn = aws_alb_listener.https_listener.arn
+  priority     = each.value.priority
+
+  condition {
+    host_header {
+      values = [each.value.host_header]
+    }
+  }
+
+  action {
+    type             = each.value.action_type
+    target_group_arn = each.value.target_group_arn
+  }
+}
