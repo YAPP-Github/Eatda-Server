f6c0e2ead97d7baeb292e33d06b81fab87b5a91b
lvalentine6 <ysr1227@naver.com>
2025-06-09 05:21:34 +0900

    feat(dev): ecs task 모듈 구현

---
+++ terraform/dev/ecs/task/main.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_task_definition" "ecs_dev_tasks" {
+  for_each = var.task_definitions
+
+  family                   = each.key
+  network_mode             = each.value.network_mode
+  cpu                      = each.value.cpu
+  memory                   = each.value.memory
+  task_role_arn            = each.value.task_role_arn
+  execution_role_arn       = each.value.execution_role_arn
+  requires_compatibilities = each.value.requires_compatibilities
+
+  container_definitions = jsonencode(var.container_definitions_map[each.key])
+
+  dynamic "volume" {
+    for_each = each.value.volumes
+    content {
+      name      = volume.value.name
+      host_path = volume.value.host_path
+    }
+  }
+}
