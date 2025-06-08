dbd146a5a48a22411e281881cdd3e0d476cca5dc
lvalentine6 <ysr1227@naver.com>
2025-06-09 05:20:48 +0900

    feat(dev): cloud-map 모듈 구현

---
+++ terraform/dev/cloud-map/outputs.tf
@@ -0,0 +1,7 @@
+output "namespace_id" {
+  value = aws_service_discovery_private_dns_namespace.time_eat_ns.id
+}
+
+output "namespace_arn" {
+  value = aws_service_discovery_private_dns_namespace.time_eat_ns.arn
+}
---
+++ terraform/dev/cloud-map/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_id" {}
+
+variable "ns_name" {
+  type = string
+}
+
+variable "ns_ttl" {
+  type = number
+}
+
+variable "ns_type" {
+  type = string
+}
+
+variable "ns_failure_threshold" {
+  type = number
+}
+
+variable "service_discovery_service_name" {
+  type = string
+}
