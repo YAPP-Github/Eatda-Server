449f5a3cac4805b626437a3d4210b80b0f97b9c8
lvalentine6 <ysr1227@naver.com>
2025-06-11 04:34:17 +0900

    feat(prod): ecs cluster 모듈 구현

---
+++ terraform/prod/ecs/cluster/variables.tf
@@ -0,0 +1,26 @@
+variable "cluster_name" {
+  description = "dev cluster name"
+  default     = "time-eat-dev-cluster"
+}
+
+variable "namespace_id" {
+  description = "dev namespace id"
+  type        = string
+}
+
+variable "cluster_settings" {
+  type = object({
+    name  = string
+    value = string
+  })
+
+  validation {
+    condition     = var.cluster_settings.name == "containerInsights"
+    error_message = "Only 'containerInsights' is supported for ECS cluster setting.name."
+  }
+}
+
+variable "tags" {
+  type = map(string)
+  default = {}
+}
