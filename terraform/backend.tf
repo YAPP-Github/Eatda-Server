69ab6de658b84dc2eb022a8dd96b54cab1f0030a
lvalentine6 <ysr1227@naver.com>
2025-06-11 19:29:11 +0900

    fix: s3 참조 버킷 수정

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "timeeat-prod-terraform-state-ap-northeast-2"
-    key            = "prod/terraform.tfstate"
+    bucket         = "timeeat-tf-state"
+    key            = "bootstrap/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
     dynamodb_table = "timeeat-terraform-lock"
