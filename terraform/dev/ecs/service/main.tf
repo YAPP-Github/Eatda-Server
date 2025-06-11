c727c8b13cec1ca43d721ecaf02e91e0f0f0db61
lvalentine6 <ysr1227@naver.com>
2025-06-12 07:10:29 +0900

    fix: 로드밸런서 옵션 추가

--- terraform/dev/ecs/service/main.tf
+++ terraform/dev/ecs/service/main.tf
@@ -1,12 +1,18 @@
 resource "aws_ecs_service" "dev" {
-  for_each = var.dev_ecs_services
+  for_each = var.ecs_services
 
   name                = each.key
   cluster             = var.cluster_id
-  launch_type         = each.value.launch_type
-  task_definition     = each.value.task_definition
+  launch_type         = var.launch_type
+  task_definition     = var.task_definition_arn
   desired_count       = each.value.desired_count
-  scheduling_strategy = each.value.scheduling_strategy
+  scheduling_strategy = var.scheduling_strategy
+
+  load_balancer {
+    target_group_arn = var.alb_target_group_arns[each.value.load_balancer.target_group_key]
+    container_name   = each.value.load_balancer.container_name
+    container_port   = each.value.load_balancer.container_port
+  }
 
   deployment_controller {
     type = var.deployment_controller_type
