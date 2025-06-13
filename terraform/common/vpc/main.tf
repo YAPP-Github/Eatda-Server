c6020568b8569cb8347279569374d0800551fbef
lvalentine6 <ysr1227@naver.com>
2025-06-13 10:02:05 +0900

    chore: 퍼블릭 서브넷 ip 자동할당 비활성화

--- terraform/common/vpc/main.tf
+++ terraform/common/vpc/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-pub-sub-${substr(var.availability_zones[count.index], -2, 2)}"
