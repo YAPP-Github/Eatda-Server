817fe538c435e71897b241de9174c2f7600b1fd7
lvalentine6 <ysr1227@naver.com>
2025-06-17 03:37:00 +0900

    fix: ECR 이름을 위한 Output 수정

--- terraform-bootstrap/outputs.tf
+++ terraform-bootstrap/outputs.tf
@@ -2,4 +2,10 @@ output "ecr_repo_names" {
   value = {
     for k, v in module.ecr : k => v.repository_url
   }
-} 
+}
+
+output "ecr_repo_name" {
+  value = {
+    for k, v in module.ecr : k => v.repository_name
+  }
+}
--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,3 +1,7 @@
 output "ecs_task_definitions_check" {
   value = local.ecs_task_definitions
 }
+
+output "ecr_repository_name" {
+  value = data.terraform_remote_state.bootstrap.outputs.ecr_repo_name["dev"]
+}
