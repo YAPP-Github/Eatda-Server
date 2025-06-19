8c1655b420541c5000c0140fb32ebd0ac29a6204
lvalentine6 <ysr1227@naver.com>
2025-06-20 05:38:57 +0900

    feat: 매장 도메인 구현

---
+++ src/main/java/timeeat/domain/store/Store.java
@@ -0,0 +1,93 @@
+package timeeat.domain.store;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import timeeat.enums.InterestArea;
+import timeeat.enums.StoreCategory;
+import timeeat.exception.BusinessErrorCode;
+import timeeat.exception.BusinessException;
+
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "store")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private StoreCategory category;
+
+    @Embedded
+    private Coordinates coordinates;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Embedded
+    private StorePhoneNumber storePhoneNumber;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Embedded
+    private StoreHours storeHours;
+
+    @Column(name = "introduction")
+    private String introduction;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "interest_area", nullable = false)
+    private InterestArea interestArea;
+
+    public Store(
+            String name,
+            String category,
+            String address,
+            Double latitude,
+            Double longitude,
+            String phoneNumber,
+            String imageUrl,
+            LocalTime openTime,
+            LocalTime closeTime,
+            String introduction,
+            String interestArea
+    ) {
+
+        validateName(name);
+        validateAddress(address);
+
+        this.name = name;
+        this.address = address;
+        this.imageUrl = imageUrl;
+        this.introduction = introduction;
+
+        this.category = StoreCategory.from(category);
+        this.interestArea = InterestArea.from(interestArea);
+        this.coordinates = new Coordinates(latitude, longitude);
+        this.storePhoneNumber = new StorePhoneNumber(phoneNumber);
+        this.storeHours = new StoreHours(openTime, closeTime);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new BusinessException(BusinessErrorCode.INVALID_STORE_NAME);
+        }
+    }
+
+    private void validateAddress(String address) {
+        if (address == null || address.trim().isEmpty()) {
+            throw new BusinessException(BusinessErrorCode.INVALID_STORE_ADDRESS);
+        }
+    }
+}
---
+++ src/main/java/timeeat/domain/store/StoreHours.java
@@ -0,0 +1,33 @@
+package timeeat.domain.store;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import timeeat.exception.BusinessErrorCode;
+import timeeat.exception.BusinessException;
+
+import java.time.LocalTime;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class StoreHours {
+
+    private LocalTime openTime;
+    private LocalTime closeTime;
+
+    public StoreHours(LocalTime openTime, LocalTime closeTime) {
+        validate(openTime, closeTime);
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+
+    private void validate(LocalTime openTime, LocalTime closeTime) {
+        if (openTime != null && closeTime != null && openTime.isAfter(closeTime)) {
+            throw new BusinessException(BusinessErrorCode.INVALID_STORE_TIME);
+        }
+    }
+}
