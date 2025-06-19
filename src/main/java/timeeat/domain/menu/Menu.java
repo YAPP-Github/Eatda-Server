8daa73c5ca2fde5e0fdec1cd47d0e88df20f1b61
lvalentine6 <ysr1227@naver.com>
2025-06-20 05:38:20 +0900

    feat: 메뉴 도메인 구현

---
+++ src/main/java/timeeat/domain/menu/Menu.java
@@ -0,0 +1,60 @@
+package timeeat.domain.menu;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import timeeat.exception.BusinessErrorCode;
+import timeeat.exception.BusinessException;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "menu")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Embedded
+    private Price price;
+
+    @Embedded
+    private Discount discount;
+
+    public Menu(
+            String name,
+            String description,
+            Integer price,
+            String imageUrl,
+            Integer discountPrice,
+            LocalDateTime discountStartTime,
+            LocalDateTime discountEndTime
+    ) {
+        validateName(name);
+
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = new Price(price);
+        this.discount = new Discount(this.price, discountPrice, discountStartTime, discountEndTime);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new BusinessException(BusinessErrorCode.INVALID_MENU_NAME);
+        }
+    }
+}
---
+++ src/main/java/timeeat/domain/menu/Price.java
@@ -0,0 +1,33 @@
+package timeeat.domain.menu;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import timeeat.exception.BusinessErrorCode;
+import timeeat.exception.BusinessException;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Price {
+
+    private static final int MIN_PRICE = 1;
+
+    @Column(name = "price", nullable = false)
+    private Integer value;
+
+    public Price(Integer value) {
+        validateMinPrice(value);
+        this.value = value;
+    }
+
+    private void validateMinPrice(Integer value) {
+        if (value == null || value < MIN_PRICE) {
+            throw new BusinessException(BusinessErrorCode.INVALID_MENU_PRICE);
+        }
+    }
+}
