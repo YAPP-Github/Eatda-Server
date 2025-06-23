7d448d53072b1131856b3e35b555a6913c0e20c2
leegwichan <cnddkscndgus@naver.com>
2025-06-23 15:19:33 +0900

    feat: 로그인 임시 API 구현

--- src/main/java/timeeat/service/member/MemberService.java
+++ src/main/java/timeeat/service/member/MemberService.java
@@ -2,9 +2,14 @@ package timeeat.service.member;
 
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import timeeat.client.oauth.OauthClient;
+import timeeat.client.oauth.OauthMemberInformation;
+import timeeat.client.oauth.OauthToken;
+import timeeat.controller.member.MemberLoginRequest;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -13,5 +18,11 @@ public class MemberService {
 
     public URI getOauthLoginUrl() {
         return oauthClient.getOauthLoginUrl();
+    }
+
+    public void login(MemberLoginRequest request) {
+        OauthToken oauthToken = oauthClient.requestOauthToken(request.code());
+        OauthMemberInformation oauthMemberInformation = oauthClient.requestMemberInformation(oauthToken);
+        log.info("Oauth 로그인 성공: {}", oauthMemberInformation); // TODO 회원 정보 저장 로직 추가
     }
 }
