bcaafd4759716796d852456105f0ad3bbf4fd900
leegwichan <cnddkscndgus@naver.com>
2025-06-24 15:22:28 +0900

    test: OAuth 리다이렉트 문서 내용 추가

--- src/test/java/timeeat/document/member/MemberDocumentTest.java
+++ src/test/java/timeeat/document/member/MemberDocumentTest.java
@@ -3,15 +3,23 @@ package timeeat.document.member;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import timeeat.document.BaseDocumentTest;
+import timeeat.document.RestDocsRequest;
+import timeeat.document.Tag;
 
 public class MemberDocumentTest extends BaseDocumentTest {
 
     @Nested
     class RedirectOauthLoginPage {
 
+        RestDocsRequest requestDocument = request()
+                .tag(Tag.MEMBER_API)
+                .summary("OAuth 로그인 페이지 리다이렉트");
+
         @Test
         void Oauth_로그인_페이지로_리다이렉트_할_수_있다() {
-            var document = document("member/oauth_redirect", 302).build();
+            var document = document("member/oauth_redirect", 302)
+                    .request(requestDocument)
+                    .build();
 
             given(document)
                     .redirects().follow(false)
