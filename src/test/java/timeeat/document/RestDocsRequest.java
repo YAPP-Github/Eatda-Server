79ba0cedcacdb12961ed81ec6b4d89c663d80830
lvalentine6 <ysr1227@naver.com>
2025-06-26 15:08:51 +0900

    style: import 컨벤션 수정

--- src/test/java/timeeat/document/RestDocsRequest.java
+++ src/test/java/timeeat/document/RestDocsRequest.java
@@ -8,7 +8,6 @@ import static org.springframework.restdocs.request.RequestDocumentation.queryPar
 
 import java.util.LinkedList;
 import java.util.List;
-
 import org.springframework.restdocs.cookies.CookieDescriptor;
 import org.springframework.restdocs.headers.HeaderDescriptor;
 import org.springframework.restdocs.payload.FieldDescriptor;
