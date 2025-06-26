6f50619f57c6eeebb9d10b3ad36fe249f9b37cf7
leegwichan <cnddkscndgus@naver.com>
2025-06-26 11:11:40 +0900

    chore: 애너테이션 순서 수정

--- src/main/java/timeeat/client/oauth/OauthMemberInformation.java
+++ src/main/java/timeeat/client/oauth/OauthMemberInformation.java
@@ -3,7 +3,8 @@ package timeeat.client.oauth;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonDeserialize(using = OauthMemberInformationDeserializer.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record OauthMemberInformation(long socialId, String nickname) {
 }
+
