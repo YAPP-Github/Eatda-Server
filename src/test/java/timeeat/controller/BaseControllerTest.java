86ef4ddfed49dc4ff8874c8d3a6a5c41dd0b03cd
leegwichan <cnddkscndgus@naver.com>
2025-06-27 23:44:17 +0900

    test: OauthClient Mocking 환경 세팅

--- src/test/java/timeeat/controller/BaseControllerTest.java
+++ src/test/java/timeeat/controller/BaseControllerTest.java
@@ -1,18 +1,25 @@
 package timeeat.controller;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.Filter;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import timeeat.DatabaseCleaner;
+import timeeat.client.oauth.OauthClient;
+import timeeat.client.oauth.OauthMemberInformation;
+import timeeat.client.oauth.OauthToken;
 import timeeat.controller.web.jwt.JwtManager;
 
 @ExtendWith(DatabaseCleaner.class)
@@ -21,20 +28,33 @@ public class BaseControllerTest {
 
     private static final List<Filter> SPEC_FILTERS = List.of(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
-    @Autowired
-    private JwtManager jwtManager;
+    private static final OauthToken DEFAULT_OAUTH_TOKEN = new OauthToken("oauth-access-token");
+    private static final OauthMemberInformation DEFAULT_OAUTH_MEMBER_INFO =
+            new OauthMemberInformation(123L, "nickname");
 
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private JwtManager jwtManager;
+
+    @MockitoBean
+    private OauthClient oauthClient;
+
     private RequestSpecification spec;
 
     @BeforeEach
-    protected final void setEnvironment() {
+    final void setEnvironment() {
         RestAssured.port = port;
         spec = new RequestSpecBuilder()
                 .addFilters(SPEC_FILTERS)
                 .build();
+    }
+
+    @BeforeEach
+    final void mockingClient() {
+        doReturn(DEFAULT_OAUTH_TOKEN).when(oauthClient).requestOauthToken(anyString());
+        doReturn(DEFAULT_OAUTH_MEMBER_INFO).when(oauthClient).requestMemberInformation(DEFAULT_OAUTH_TOKEN);
     }
 
     protected final RequestSpecification given() {
--- src/test/java/timeeat/service/BaseServiceTest.java
+++ src/test/java/timeeat/service/BaseServiceTest.java
@@ -1,11 +1,31 @@
 package timeeat.service;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import timeeat.DatabaseCleaner;
+import timeeat.client.oauth.OauthClient;
+import timeeat.client.oauth.OauthMemberInformation;
+import timeeat.client.oauth.OauthToken;
 
 @ExtendWith(DatabaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public abstract class BaseServiceTest {
 
+    private static final OauthToken DEFAULT_OAUTH_TOKEN = new OauthToken("oauth-access-token");
+    private static final OauthMemberInformation DEFAULT_OAUTH_MEMBER_INFO =
+            new OauthMemberInformation(123L, "nickname");
+
+    @MockitoBean
+    private OauthClient oauthClient;
+
+    @BeforeEach
+    protected final void mockingClient() {
+        doReturn(DEFAULT_OAUTH_TOKEN).when(oauthClient).requestOauthToken(anyString());
+        doReturn(DEFAULT_OAUTH_MEMBER_INFO).when(oauthClient).requestMemberInformation(DEFAULT_OAUTH_TOKEN);
+    }
 }
