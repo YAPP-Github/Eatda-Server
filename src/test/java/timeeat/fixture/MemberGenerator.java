3cc44ca05672c5cb177cb1a11f941c16adb10291
leegwichan <cnddkscndgus@naver.com>
2025-07-02 17:02:06 +0900

    feat: 닉네임, 핸드폰 번호 검증 기능 구현

--- src/test/java/timeeat/fixture/MemberGenerator.java
+++ src/test/java/timeeat/fixture/MemberGenerator.java
@@ -2,10 +2,13 @@ package timeeat.fixture;
 
 import org.springframework.stereotype.Component;
 import timeeat.domain.member.Member;
+import timeeat.enums.InterestArea;
 import timeeat.repository.member.MemberRepository;
 
 @Component
 public class MemberGenerator {
+
+    private static final String DEFAULT_INTEREST_AREA = InterestArea.SEONGBUK.getAreaName();
 
     private final MemberRepository memberRepository;
 
@@ -19,5 +22,9 @@ public class MemberGenerator {
 
     public Member generate(String socialId, String nickname) {
         return memberRepository.save(new Member(socialId, nickname));
+    }
+
+    public Member generateRegisteredMember(String socialId, String nickname, String phoneNumber) {
+        return memberRepository.save(new Member(socialId, nickname, phoneNumber, DEFAULT_INTEREST_AREA, true));
     }
 }
--- src/test/java/timeeat/service/service/MemberServiceTest.java
+++ src/test/java/timeeat/service/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package timeeat.service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,6 +19,76 @@ class MemberServiceTest extends BaseServiceTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Nested
+    class ValidateNickname {
+
+        @Test
+        void 중복되지_않은_닉네임이면_예외가_발생하지_않는다() {
+            memberGenerator.generate("123", "nickname");
+            Member member = memberGenerator.generate("456", "unique-nickname");
+            String newNickname = "new-unique-nickname";
+
+            assertThatCode(() -> memberService.validateNickname(newNickname, member.getId()))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 자신의_기존_닉네임이면_예외가_발생하지_않는다() {
+            Member member = memberGenerator.generate("123", "nickname");
+            String newNickname = "nickname";
+
+            assertThatCode(() -> memberService.validateNickname(newNickname, member.getId()))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 중복된_닉네임이_있으면_예외가_발생한다() {
+            memberGenerator.generate("123", "duplicate-nickname");
+            Member member = memberGenerator.generate("456", "another-nickname");
+            String newNickname = "duplicate-nickname";
+
+            BusinessException exception = assertThrows(BusinessException.class,
+                    () -> memberService.validateNickname(newNickname, member.getId()));
+
+            assertThat(exception.getErrorCode()).isEqualTo(BusinessErrorCode.DUPLICATE_NICKNAME);
+        }
+    }
+
+    @Nested
+    class validatePhoneNumber {
+
+        @Test
+        void 중복되지_않은_전화번호이면_예외가_발생하지_않는다() {
+            memberGenerator.generate("123", "nickname");
+            Member member = memberGenerator.generate("456", "unique-nickname");
+            String newPhoneNumber = "01012345678";
+
+            assertThatCode(() -> memberService.validatePhoneNumber(newPhoneNumber, member.getId()))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 자신의_기존_전화번호이면_예외가_발생하지_않는다() {
+            Member member = memberGenerator.generateRegisteredMember("123", "nickname", "01012345678");
+            String newPhoneNumber = "01012345678";
+
+            assertThatCode(() -> memberService.validatePhoneNumber(newPhoneNumber, member.getId()))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 중복된_전화번호가_있으면_예외가_발생한다() {
+            memberGenerator.generateRegisteredMember("123", "nickname1", "01012345678");
+            Member member = memberGenerator.generateRegisteredMember("456", "nickname2", "01087654321");
+            String newPhoneNumber = "01012345678";
+
+            BusinessException exception = assertThrows(BusinessException.class,
+                    () -> memberService.validatePhoneNumber(newPhoneNumber, member.getId()));
+
+            assertThat(exception.getErrorCode()).isEqualTo(BusinessErrorCode.DUPLICATE_PHONE_NUMBER);
+        }
+    }
 
     @Nested
     class Update {
@@ -66,9 +137,7 @@ class MemberServiceTest extends BaseServiceTest {
         @Test
         void 중복된_전화번호가_있으면_예외가_발생한다() {
             String phoneNumber = "01012345678";
-            Member existMember = memberGenerator.generate("123", "nickname1");
-            memberService.update(existMember.getId(),
-                    new MemberUpdateRequest("nickname1", phoneNumber, "성북구", true));
+            memberGenerator.generateRegisteredMember("123", "nickname1", phoneNumber);
             Member updatedMember = memberGenerator.generate("456", "nickname2");
             MemberUpdateRequest request =
                     new MemberUpdateRequest("new-nickname", phoneNumber, "성북구", true);
@@ -82,9 +151,7 @@ class MemberServiceTest extends BaseServiceTest {
         @Test
         void 기존의_전화번호와_동일하면_정상적으로_회원_정보가_수정된다() {
             String phoneNumber = "01012345678";
-            Member member = memberGenerator.generate("123", "nickname1");
-            memberService.update(member.getId(),
-                    new MemberUpdateRequest("nickname1", phoneNumber, "성북구", true));
+            Member member = memberGenerator.generateRegisteredMember("123", "nickname1", phoneNumber);
             MemberUpdateRequest request =
                     new MemberUpdateRequest("new-nickname", phoneNumber, "성북구", true);
 
