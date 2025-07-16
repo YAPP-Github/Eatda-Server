089df85cfb7a7d276b818f2880caf51372137a1e
lvalentine6 <ysr1227@naver.com>
2025-07-16 23:32:23 +0900

    fix: 스토리에 스토어 정보를 들고있게 수정

--- src/main/resources/db/migration/V3__add_story_table.sql
+++ src/main/resources/db/migration/V3__add_story_table.sql
@@ -1,10 +1,13 @@
 CREATE TABLE `story`
 (
-    `id`          BIGINT       NOT NULL AUTO_INCREMENT,
-    `member_id`   BIGINT       NOT NULL,
-    `store_id`    BIGINT       NOT NULL,
-    `description` TEXT         NOT NULL,
-    `image_key`   VARCHAR(511) NOT NULL,
-    `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `id`             BIGINT       NOT NULL AUTO_INCREMENT,
+    `member_id`      BIGINT       NOT NULL,
+    `store_kakao_id` VARCHAR(255) NOT NULL,
+    `store_name`     VARCHAR(255) NOT NULL,
+    `store_address`  VARCHAR(255) NOT NULL,
+    `store_category` VARCHAR(50)  NOT NULL,
+    `description`    TEXT         NOT NULL,
+    `image_key`      VARCHAR(511) NOT NULL,
+    `created_at`     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
