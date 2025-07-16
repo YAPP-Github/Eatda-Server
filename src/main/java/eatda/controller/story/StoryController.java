510349226c774217524c8d57d1268eff86284563
lvalentine6 <ysr1227@naver.com>
2025-07-17 00:17:43 +0900

    fix: 미구현 메서드 삭제

--- src/main/java/eatda/controller/story/StoryController.java
+++ src/main/java/eatda/controller/story/StoryController.java
@@ -5,9 +5,7 @@ import eatda.service.story.StoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,17 +24,5 @@ public class StoryController {
     ) {
         storyService.registerStory(request, image, member.id());
         return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @GetMapping("/api/stories")
-    public ResponseEntity<Void> getStories() {
-
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/api/story")
-    public ResponseEntity<Void> getStory(@RequestParam Long storyId) {
-
-        return ResponseEntity.noContent().build();
     }
 }
