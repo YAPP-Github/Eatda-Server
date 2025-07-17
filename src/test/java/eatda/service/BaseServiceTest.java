e527cf526f6a1c7f6596389d333b9c7793210368
lvalentine6 <ysr1227@naver.com>
2025-07-18 01:27:25 +0900

    fix: StoryRepository 실제 객체 사용으로 수정

--- src/test/java/eatda/service/BaseServiceTest.java
+++ src/test/java/eatda/service/BaseServiceTest.java
@@ -35,6 +35,6 @@ public abstract class BaseServiceTest {
     @MockitoBean
     protected ImageService imageService;
 
-    @MockitoBean
+    @Autowired
     protected StoryRepository storyRepository;
 }
