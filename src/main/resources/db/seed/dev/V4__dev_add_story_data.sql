f83b17c5226526ce616cd58552a54f65ff3aaed7
lvalentine6 <ysr1227@naver.com>
2025-07-18 20:38:41 +0900

    fix: 더미 데이터에 실제 이미지가 나가도록 key 수정

--- src/main/resources/db/seed/dev/V4__dev_add_story_data.sql
+++ src/main/resources/db/seed/dev/V4__dev_add_story_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO story (member_id, store_kakao_id, store_name, store_address,
                    store_category, description, image_key)
-VALUES (1, '99999999999', '맛있는 한식집', '서울시 강남구 역삼동 123-45', 'KOREAN', '진짜 여기 곱창 맛집임. 다시 또 갈 듯!', 'story/preview/1.jpg'),
-       (2, '99999999998', '아름다운 양식집', '서울시 강남구 역삼동 67-89', 'WESTERN', '스테이크가 부드럽고 서비스도 좋아요.', 'story/preview/2.jpg'),
-       (3, '99999999997', '정통 중식당', '서울시 강남구 역삼동 101-112', 'CHINESE', '짜장면이 정통의 맛. 강력 추천.', 'story/preview/3.jpg');
+VALUES (1, '99999999999', '맛있는 한식집', '서울시 강남구 역삼동 123-45', 'KOREAN', '진짜 여기 곱창 맛집임. 다시 또 갈 듯!', 'story/dummy/1.jpg'),
+       (2, '99999999998', '아름다운 양식집', '서울시 강남구 역삼동 67-89', 'WESTERN', '스테이크가 부드럽고 서비스도 좋아요.', 'story/dummy/2.jpg'),
+       (3, '99999999997', '정통 중식당', '서울시 강남구 역삼동 101-112', 'CHINESE', '짜장면이 정통의 맛. 강력 추천.', 'story/dummy/3.jpg');
