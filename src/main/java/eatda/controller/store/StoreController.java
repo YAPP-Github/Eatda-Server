7afaf90b1fd2002c9f985cab9fa998c05ce39d06
leegwichan <cnddkscndgus@naver.com>
2025-07-18 17:32:17 +0900

    feat: 최신 가게 조회 API 구현

--- src/main/java/eatda/controller/store/StoreController.java
+++ src/main/java/eatda/controller/store/StoreController.java
@@ -2,6 +2,8 @@ package eatda.controller.store;
 
 import eatda.controller.web.auth.LoginMember;
 import eatda.service.store.StoreService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreController {
 
     private final StoreService storeService;
+
+    @GetMapping("/api/shops")
+    public ResponseEntity<StoresResponse> getStores(@RequestParam @Min(1) @Max(50) int size) {
+        return ResponseEntity.ok(storeService.getStores(size));
+    }
 
     @GetMapping("/api/shop/search")
     public ResponseEntity<StoreSearchResponses> searchStore(@RequestParam String query, LoginMember member) {
