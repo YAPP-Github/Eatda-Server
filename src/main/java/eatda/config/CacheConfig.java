b2171019025b7bd5f61d0f829bd833dccc1e3b39
leegwichan <cnddkscndgus@naver.com>
2025-07-19 14:35:20 +0900

    fix(CacheConfig): 빈 설정 파일에서 `@Configuration` 을 사용하도록 변경

--- src/main/java/eatda/config/CacheConfig.java
+++ src/main/java/eatda/config/CacheConfig.java
@@ -8,9 +8,9 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class CacheConfig {
 
     @Bean
