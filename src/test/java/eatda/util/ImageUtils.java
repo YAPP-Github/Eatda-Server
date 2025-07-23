571a7dd5262f0caa781cb76278564449dba23465
leegwichan <cnddkscndgus@naver.com>
2025-07-23 11:29:47 +0900

    test: `ImageUtils` 에러 처리 개선

--- src/test/java/eatda/util/ImageUtils.java
+++ src/test/java/eatda/util/ImageUtils.java
@@ -7,14 +7,16 @@ import java.io.FileNotFoundException;
 
 public final class ImageUtils {
 
+    private static final String TEST_IMAGE_PATH = "classpath:test/test-image.png";
+
     private ImageUtils() {
     }
 
     public static File getTestImage() {
         try {
-            return getFile("classpath:test/test-image.png");
+            return getFile(TEST_IMAGE_PATH);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("테스트 이미지 파일을 찾을 수 없습니다: " + TEST_IMAGE_PATH, e);
         }
     }
 }
