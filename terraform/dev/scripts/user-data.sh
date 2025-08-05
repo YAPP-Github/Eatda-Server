53ed6ed01470e2bd2b1f9f11c3f96341e3216da5
lvalentine6 <ysr1227@naver.com>
2025-08-06 02:05:57 +0900

    feat: dev mysql 컨테이너 백업 추가

--- terraform/dev/scripts/user-data.sh
+++ terraform/dev/scripts/user-data.sh
@@ -12,6 +12,9 @@ echo '/swapfile none swap sw 0 0' >> /etc/fstab
 aws s3 cp s3://eatda-storage-dev/scripts/app-backup-dev-logs.sh /home/ec2-user/logs/eatda/app-backup-dev-logs.sh
 chmod +x /home/ec2-user/logs/eatda/app-backup-dev-logs.sh
 
+aws s3 cp s3://eatda-storage-dev/scripts/mysql-backup.sh /home/ec2-user/eatda/mysql/mysql-backup.sh
+chmod +x /home/ec2-user/eatda/mysql/mysql-backup.sh
+
 yum install -y cronie
 systemctl enable crond
 systemctl start crond
@@ -21,3 +24,4 @@ until systemctl is-active --quiet crond; do
 done
 
 (crontab -l 2>/dev/null; echo "0 0 * * 0 /home/ec2-user/logs/eatda/app-backup-dev-logs.sh >> /var/log/app-backup.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "30 0 * * 0 /home/ec2-user/eatda/mysql/mysql-backup.sh >> /var/log/mysql-backup.log 2>&1") | crontab -
