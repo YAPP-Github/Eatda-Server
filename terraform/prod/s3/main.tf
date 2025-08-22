6ca8ee38a7fe107dcf418fea9116af114c19f0f6
lvalentine6 <ysr1227@naver.com>
2025-08-22 22:44:21 +0900

    fix(prod): s3 CORS PUT 추가

--- terraform/prod/s3/main.tf
+++ terraform/prod/s3/main.tf
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_cors_configuration" "prod" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
+    allowed_methods = ["GET", "PUT"]
     allowed_origins = var.allowed_origins
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
