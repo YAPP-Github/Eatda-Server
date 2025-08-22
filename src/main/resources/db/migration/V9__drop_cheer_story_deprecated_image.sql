96de7d75906f9ffae6734fa9f9aea72dff9d9302
lvalentine6 <ysr1227@naver.com>
2025-08-22 23:16:19 +0900

    fix: deprecated_image_key 조건부 삭제하도록 변경

--- src/main/resources/db/migration/V9__drop_cheer_story_deprecated_image.sql
+++ src/main/resources/db/migration/V9__drop_cheer_story_deprecated_image.sql
@@ -1,4 +1,26 @@
-ALTER TABLE cheer
-    DROP COLUMN _deprecated_image_key;
-ALTER TABLE story
-    DROP COLUMN _deprecated_image_key;
+SET @col_exists := (SELECT COUNT(*)
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE TABLE_SCHEMA = DATABASE()
+                      AND TABLE_NAME = 'cheer'
+                      AND COLUMN_NAME = '_deprecated_image_key');
+
+SET @sql := IF(@col_exists > 0,
+               'ALTER TABLE cheer DROP COLUMN _deprecated_image_key',
+               'SELECT "Column cheer._deprecated_image_key does not exist, skip";');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+
+SET @col_exists := (SELECT COUNT(*)
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE TABLE_SCHEMA = DATABASE()
+                      AND TABLE_NAME = 'story'
+                      AND COLUMN_NAME = '_deprecated_image_key');
+
+SET @sql := IF(@col_exists > 0,
+               'ALTER TABLE story DROP COLUMN _deprecated_image_key',
+               'SELECT "Column story._deprecated_image_key does not exist, skip";');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
