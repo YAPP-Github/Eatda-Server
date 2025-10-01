92786cee9cca6114b99d8ead5d8b81fa5bed9d1c
lvalentine6 <ysr1227@naver.com>
2025-10-01 21:06:11 +0900

    fix: 브라우저 허용 규칙 강화

--- terraform/common/waf/main.tf
+++ terraform/common/waf/main.tf
@@ -85,32 +85,32 @@ resource "aws_wafv2_web_acl" "this" {
     statement {
       or_statement {
         statement {
-          size_constraint_statement {
+          byte_match_statement {
             field_to_match {
               single_header {
                 name = "origin"
               }
             }
-            comparison_operator = "GT"
-            size                = 0
+            search_string         = "eatda.net"
+            positional_constraint = "ENDS_WITH"
             text_transformation {
               priority = 0
-              type     = "NONE"
+              type     = "LOWERCASE"
             }
           }
         }
         statement {
-          size_constraint_statement {
+          byte_match_statement {
             field_to_match {
               single_header {
                 name = "referer"
               }
             }
-            comparison_operator = "GT"
-            size                = 0
+            search_string         = "eatda.net"
+            positional_constraint = "CONTAINS"
             text_transformation {
               priority = 0
-              type     = "NONE"
+              type     = "LOWERCASE"
             }
           }
         }
