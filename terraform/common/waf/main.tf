ab0a79df3fb24726d1cbb192784af43263d5e30e
lvalentine6 <ysr1227@naver.com>
2025-10-07 23:43:08 +0900

    fix: dev 서버 로컬에서 접속 가능하도록 수정

--- terraform/common/waf/main.tf
+++ terraform/common/waf/main.tf
@@ -114,6 +114,66 @@ resource "aws_wafv2_web_acl" "this" {
             }
           }
         }
+        statement {
+          byte_match_statement {
+            field_to_match {
+              single_header {
+                name = "origin"
+              }
+            }
+            search_string         = "dev.eatda.net"
+            positional_constraint = "ENDS_WITH"
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+        statement {
+          byte_match_statement {
+            field_to_match {
+              single_header {
+                name = "referer"
+              }
+            }
+            search_string         = "dev.eatda.net"
+            positional_constraint = "CONTAINS"
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+        statement {
+          byte_match_statement {
+            field_to_match {
+              single_header {
+                name = "origin"
+              }
+            }
+            search_string         = "http://localhost:3000"
+            positional_constraint = "EXACTLY"
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
+        statement {
+          byte_match_statement {
+            field_to_match {
+              single_header {
+                name = "referer"
+              }
+            }
+            search_string         = "http://localhost:3000/"
+            positional_constraint = "STARTS_WITH"
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
       }
     }
     visibility_config {
